b47f0af36f782e9d6843bd95ec9cfaba67528c77
umitliguler <info.umitguler@gmail.com>
2018-06-17 12:32:45 +0300

    first

---
+++ PolimorfizmDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolimorfizmDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bot bot = new Bot();
+            Cizme cizme = new Cizme();
+
+            AyakkabiKalibi ayakkabi1 = new Bot();
+            AyakkabiKalibi ayakkabi2 = new Cizme();
+
+            bot.SuGecirebilir = true;
+
+            IAyakkabi iAyakkabi = new Bot();
+
+        }
+    }
+}
